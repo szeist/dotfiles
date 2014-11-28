37e7cbe38bf546e741aa9e3d1869805caf9f5701
Istvan Szenasi <szeist@gmail.com>
2014-11-28 13:06:38 +0100

    Add trayer-srg

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,17 +14,25 @@ myLayouts = avoidStruts $
     onWorkspaces [ "web", "mail" ] Full $
     layoutHook defaultConfig
 
+myManageHook = manageDocks <+> manageHook defaultConfig
+
+myStartupHook :: X()
+myStartupHook = do
+    spawn "trayer-srg --edge top --align right --width 5 --height 18 --transparent true --alpha 0 --tint 0x101010 --SetDockType true --SetPartialStrut true"
+
 main = do
     -- Start xmobar
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
         workspaces = myWorkspaces,
         layoutHook = myLayouts,
+        manageHook = myManageHook,
         -- Print status info and xmobar on state updates
         logHook = dynamicLogWithPP xmobarPP {
             ppOutput = hPutStrLn xmproc,
             ppTitle = xmobarColor "green" "" . shorten 50
         },
+        startupHook = myStartupHook,
         -- Use super key instead of alt
         modMask = mod4Mask
     }
