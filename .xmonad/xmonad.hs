2b867e1dd04aea1c8389a1c9b755665f75f0f905
Istvan Szenasi <szeist@gmail.com>
2014-12-07 23:24:52 +0100

    Draw gb image when xmonad starts, pulseaudio support in binding keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,27 +2,54 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Layout.PerWorkspace
+import XMonad.Layout.SimpleFloat
 import System.IO
+import Control.Monad (when)
+import System.Directory (doesFileExist)
+import Graphics.X11.Xinerama (getScreenInfo)
+import Graphics.ImageMagick.MagickWand
+import Filesystem.Path.CurrentOS (decodeString)
 
 -- Workspaces
-myWorkspaces = [ "main", "web", "terminals", "mail", "im" ]
+myWorkspaces :: [ String ]
+myWorkspaces = [ "main", "web", "terminals", "mail", "im", "float" ]
 
 -- Workspace layouts
 myLayouts = avoidStruts $
     onWorkspaces [ "web", "mail" ] Full $
+    onWorkspace  "float" simpleFloat $
     layoutHook defaultConfig
 
-myManageHook = manageDocks <+> manageHook defaultConfig
+myManageHook :: ManageHook
+myManageHook = composeAll [
+        className =? "spotify" --> doFloat,
+        className =? "Chromium-browser" --> doShift "web",
+        className =? "Firefox" --> doShift "web",
+        className =? "Thunderbird" --> doShift "mail",
+        className =? "Skype" --> doShift "im"
+    ]
+    <+> manageDocks
+    <+> manageHook defaultConfig
 
+-- Startup programs
 myStartupHook :: X()
 myStartupHook = do
     spawn "trayer-srg --edge top --align right --width 5 --height 18 --transparent true --alpha 0 --tint 0x101010 --SetDockType true --SetPartialStrut true"
+    spawn "xbindkeys"
 
+-- Additional key mappings
+myKeys :: [ (String, X ()) ]
+myKeys = [ ("M-q", spawn "killall trayer-srg xbindkeys; xmonad --restart") ]
+
+main :: IO ()
 main = do
     -- Start xmobar
     xmproc <- spawnPipe "xmobar"
+    -- Draw background image to all screen
+    doesFileExist ".Xbackground.png" >>= flip when (drawBackground ".Xbackground.png")
+
     xmonad $ defaultConfig {
         workspaces = myWorkspaces,
         layoutHook = myLayouts,
@@ -35,4 +62,41 @@ main = do
         startupHook = myStartupHook,
         -- Use super key instead of alt
         modMask = mod4Mask
-    }
+    } `additionalKeysP` myKeys
+
+-- Draw centered background image to all screens
+drawBackground :: String -> IO ()
+drawBackground backgroundImage = do
+    (imageWidth, imageHeight) <- getImageDims backgroundImage
+    screens <- openDisplay "" >>= getScreenInfo
+
+    spawn $ "xloadimage -onroot -border black -fullscreen "
+        ++ unwords (map (getOffsetArgs (imageWidth, imageHeight) backgroundImage) screens)
+
+-- Get offset xloadimage args for screen image centering
+getOffsetArgs :: (Int, Int) -> String -> Rectangle -> String
+getOffsetArgs (imageWidth, imageHeight) backgroundImage screenInfo =
+    "-at "
+    ++ getOffsetStr (getCenterImageOffsets (imageWidth, imageHeight) screenInfo)
+    ++ " "
+    ++ backgroundImage
+
+-- Format image offset string
+getOffsetStr :: (Int, Int) -> String
+getOffsetStr (offsetX, offsetY) = show offsetX ++ "," ++ show offsetY
+
+-- Calculate screen center image offsets
+getCenterImageOffsets :: (Int, Int) -> Rectangle -> (Int, Int)
+getCenterImageOffsets (imageWidth, imageHeight) screenInfo =
+    ( (fromIntegral (rect_width screenInfo) - imageWidth) `div` 2 + fromIntegral (rect_x screenInfo),
+      (fromIntegral (rect_height screenInfo) - imageHeight) `div` 2 + fromIntegral (rect_y screenInfo) )
+
+getImageDims :: String -> IO (Int, Int)
+getImageDims fileName = withMagickWandGenesis $ do
+    (_,wand) <- magickWand
+
+    readImage wand $ decodeString fileName
+    width <- getImageWidth wand
+    height <- getImageHeight wand
+
+    return (width, height)
