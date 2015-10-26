5df276911bfe86c5de5eb6c2d850c12d26a88287
Istvan Szenasi <szeist@gmail.com>
2015-10-26 08:04:21 +0100

    xmonad: split comfiguration

---
+++ .xmonad/.vimrc_local.vim
@@ -0,0 +1,1 @@
+let g:syntastic_haskell_hdevtools_args = "--ghcopts '-i lib'"
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,51 +7,47 @@ import XMonad.Layout.PerWorkspace
 import XMonad.Layout.SimpleFloat
 import System.IO
 import Control.Monad (when)
-import Control.Applicative((<$>))
 import System.Directory (doesFileExist)
-import Graphics.X11.Xinerama (getScreenInfo)
-import Graphics.ImageMagick.MagickWand
-import Filesystem.Path.CurrentOS (decodeString)
 import XMonad.Hooks.SetWMName
 
--- Workspaces
-myWorkspaces :: [ String ]
-myWorkspaces = [ "main", "web", "terminals", "mail", "im", "float" ]
+import Utils.BackgroundImage
+import Utils.Screen
 
--- Workspace layouts
+myWorkspaces :: [ String ]
+myWorkspaces = [ "main", "web", "work", "terminals", "im", "float" ]
+
 myLayouts = avoidStruts $
-    onWorkspaces [ "web", "mail" ] Full $
+    onWorkspaces [ "web", "work" ] Full $
     onWorkspace  "float" simpleFloat $
     layoutHook defaultConfig
 
 myManageHook :: ManageHook
 myManageHook = composeAll [
         className =? "spotify" --> doFloat,
-        className =? "Chromium-browser" --> doShift "web",
+        className =? "google-chrome" --> doShift "web",
         className =? "Firefox" --> doShift "web",
-        className =? "Thunderbird" --> doShift "mail",
         (className =? "Skype" <||> className =? "HipChat") --> doShift "im"
     ]
     <+> manageDocks
     <+> manageHook defaultConfig
 
--- Startup programs
 myStartupHook :: X()
 myStartupHook = do
+    setWMName "LG3D"
     spawn "trayer-srg --edge top --align right --width 5 --height 18 --transparent true --alpha 0 --tint 0x101010 --SetDockType true --SetPartialStrut true"
     spawn "xbindkeys"
-    setWMName "LG3D"
 
--- Additional key mappings
 myKeys :: [ (String, X ()) ]
-myKeys = [ ("M-q", spawn "killall trayer-srg xbindkeys conky; xmonad --restart") ]
+myKeys =
+  [ ("M-q", spawn "killall trayer-srg xbindkeys conky; xmonad --restart"),
+    ("M-S-l", spawn "slock")
+  ]
 
 main :: IO ()
 main = do
-    -- Start xmobar
     xmproc <- spawnPipe "xmobar"
-    -- Draw background image to all screen
-    doesFileExist ".Xbackground.png" >>= flip when (drawBackground ".Xbackground.png")
+
+    doesFileExist ".Xbackground.png" >>= flip when (drawCenteredBackground ".Xbackground.png")
 
     spawnConky 45 250
 
@@ -60,56 +56,13 @@ main = do
         layoutHook = myLayouts,
         manageHook = myManageHook,
         focusedBorderColor = "#00A300",
-        -- Print status info and xmobar on state updates
         logHook = dynamicLogWithPP xmobarPP {
             ppOutput = hPutStrLn xmproc,
             ppTitle = xmobarColor "green" "" . shorten 50
         },
         startupHook = myStartupHook,
-        -- Use super key instead of alt
         modMask = mod4Mask
     } `additionalKeysP` myKeys
 
--- Draw centered background image to all screens
-drawBackground :: String -> IO ()
-drawBackground backgroundImage = do
-    (imageWidth, imageHeight) <- getImageDims backgroundImage
-    screens <- openDisplay "" >>= getScreenInfo
-
-    spawn $ "xloadimage -onroot -border black -fullscreen "
-        ++ unwords (map (getOffsetArgs (imageWidth, imageHeight) backgroundImage) screens)
-
--- Get offset xloadimage args for screen image centering
-getOffsetArgs :: (Int, Int) -> String -> Rectangle -> String
-getOffsetArgs (imageWidth, imageHeight) backgroundImage screenInfo =
-    "-at "
-    ++ getOffsetStr (getCenterImageOffsets (imageWidth, imageHeight) screenInfo)
-    ++ " "
-    ++ backgroundImage
-
--- Format image offset string
-getOffsetStr :: (Int, Int) -> String
-getOffsetStr (offsetX, offsetY) = show offsetX ++ "," ++ show offsetY
-
--- Calculate screen center image offsets
-getCenterImageOffsets :: (Int, Int) -> Rectangle -> (Int, Int)
-getCenterImageOffsets (imageWidth, imageHeight) screenInfo =
-    ( (fromIntegral (rect_width screenInfo) - imageWidth) `div` 2 + fromIntegral (rect_x screenInfo),
-      (fromIntegral (rect_height screenInfo) - imageHeight) `div` 2 + fromIntegral (rect_y screenInfo) )
-
-getImageDims :: String -> IO (Int, Int)
-getImageDims fileName = withMagickWandGenesis $ do
-    (_,wand) <- magickWand
-
-    readImage wand $ decodeString fileName
-    width <- getImageWidth wand
-    height <- getImageHeight wand
-
-    return (width, height)
-
 spawnConky :: Int -> Int -> IO()
-spawnConky topOffset rightOffset = getScreenDims 0 >>= \dims -> spawn $ "conky -x " ++ show (fst dims - rightOffset) ++ " -y " ++ show topOffset
-
-getScreenDims :: Int -> IO (Int, Int)
-getScreenDims screenIdx = get_dims <$> (!! screenIdx) <$> (openDisplay "" >>= getScreenInfo)
-  where get_dims rect = (fromIntegral $ rect_width rect, fromIntegral $ rect_height rect)
+spawnConky topOffset rightOffset = getScreenDimensions 0 >>= \dims -> spawn $ "conky -x " ++ show (fst dims - rightOffset) ++ " -y " ++ show topOffset
