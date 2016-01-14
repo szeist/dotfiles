b905abc758d2f91f01ccf4ffe3c6561cdda78e44
Istvan Szenasi <szeist@gmail.com>
2016-01-15 00:29:10 +0100

    xmonad: add imagemagick to install script

--- .xmonad/install.sh
+++ .xmonad/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependencies
+cabal install imagemagick
+
 cabal install xmonad
 cabal install xmonad-contrib
 cabal install xmobar --flags="with_xft with_utf8 with_iwlib with_alsa with_datezone with_xmp with_threaded"
