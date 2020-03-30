f399cc5a9790fb6843e81b48ae28902d399a526a
Istvan Szenasi <szeist@gmail.com>
2020-03-31 00:40:20 +0200

    fix taskwarrior modify hook

--- .task/hooks/on-modify.py
+++ .task/hooks/on-modify.py
@@ -4,28 +4,18 @@ from __future__ import print_function
 import sys
 import json
 
-TODO_LIST_ID = "5d222b2dc62053481efa1504"
 TODO_LIST_NAME = "To Do"
-DOING_LIST_ID = "5d222b2dc62053481efa1505"
 DOING_LIST_NAME = "Doing"
-
-def _is_in_todo_list(task):
-    return task['intheamtrellolistid'] == TODO_LIST_ID
-
-def _is_in_doing_list(task):
-    return task['intheamtrellolistid'] == DOING_LIST_ID
 
 def _is_started(task):
     return 'start' in task
 
 def _move_to_doing_list(task):
-    task['intheamtrellolistid'] = DOING_LIST_ID
-    task['intheamtrellolistname'] = DOING_LIST_NAME
+    dict(task)['intheamtrellolistname'] = DOING_LIST_NAME
     return task
 
 def _move_to_todo_list(task):
-    task['intheamtrellolistid'] = TODO_LIST_ID
-    task['intheamtrellolistname'] = TODO_LIST_NAME
+    dict(task)['intheamtrellolistname'] = TODO_LIST_NAME
     return task
 
 def main():
